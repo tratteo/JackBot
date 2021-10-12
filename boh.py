faca0c8426471d9a303f6ed8a5f3de6edea9b677
Matteo Beltrame <bltmtt@gmail.com>
2021-10-13 00:21:58 +0200

    Core

--- boh.py
+++
@@ -1,7 +0,0 @@
-import talib
-print(talib.__ta_version__)
-
-from talib import abstract
-stoch =  abstract.Function('stoch')
-stoch.set_parameters(fastk_period=14, slowk_period=1, slowd_period=3, slowk_matype=0, slowd_matype=0)
-print(stoch.lookback)
