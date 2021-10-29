6c1deddda8fdd6d32557aaad8094b4b0fe3046d2
enrico-car <car.enrico19@gmail.com>
2021-10-29 12:22:38 +0200

    commanhandler, evaluate, train

--- evaluate.py
+++ evaluate.py
@@ -5,6 +5,7 @@ import config
 from numpy import genfromtxt
 
 from bot import dataset_evaluator
+from bot.command.commandHandler import CommandHandler
 from strategies.StochRsiMacdStrategy import *
 
 
@@ -16,25 +17,38 @@ def __try_get_json_attr(key: str, json_obj):
         return None
 
 
-if len(sys.argv) < 2:
-    print("Few arguments")
-    sys.exit(1)
+def helper(helperstr: str):
+    print(helperstr,flush=True)
+    exit(0)
 
-if sys.argv[1] == "-h":
-    print("evaluate <strategy_file_path> <dataset_path>")
-    sys.exit(0)
+def failure(helperstr: str):
+    print('Wrong synthax \n')
+    print(helperstr,flush=True)
+    exit(1)
 
-if len(sys.argv) < 2:
-    print("Wrong parameters, see usage with -h")
-    sys.exit()
 
-with open(sys.argv[1]) as file:
+command_manager = CommandHandler.create() \
+    .positional('Strategy file') \
+    .positional('Dataset file') \
+    .keyed('-o', 'Output file .res') \
+    .on_help(helper)\
+    .on_fail(failure) \
+    .build(sys.argv)
+
+
+with open(command_manager.get_p(0)) as file:
     data = json.load(file)
 
-dataset = sys.argv[2]
+dataset = command_manager.get_p(1)
 strategy_name = data["strategy"]
 strategy_class = getattr(importlib.import_module("strategies." + strategy_name), strategy_name)
 strategy = strategy_class(TestWallet.factory(), *data["parameters"])
 print("Evaluating...")
 res, index = dataset_evaluator.evaluate(strategy, 1000, genfromtxt(dataset, delimiter=config.DEFAULT_DELIMITER), None)
 print(res)
+
+out = command_manager.get_k('-o')
+
+if out is not None:
+    with open(out, 'w') as file:
+        file.write(str(res))
--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 import config
+from bot.command.commandHandler import CommandHandler
 from bot.training.genetic_trainer import GeneticTrainer, Gene
 from strategies.StochRsiMacdStrategy import *
 
@@ -15,33 +16,44 @@ def __try_get_json_attr(key: str, json_obj):
         return None
 
 
-if len(sys.argv) == 1 or sys.argv[1] == "-h":
-    print("train-genetic <options_file_path> <dataset> -results_folder-")
-    sys.exit(0)
+def helper(helperstr: str):
+    print(helperstr, flush=True)
+    exit(0)
 
-if len(sys.argv) < 3:
-    print("Wrong parameters see usage with -h")
-    sys.exit()
 
-with open(sys.argv[1]) as file:
+def failure(helperstr: str):
+    print('Wrong synthax \n')
+    print(helperstr, flush=True)
+    exit(1)
+
+
+command_manager = CommandHandler.create() \
+    .positional('Genetic paramenters') \
+    .positional('Dataset file') \
+    .keyed('-o', 'Output file .res') \
+    .on_help(helper) \
+    .on_fail(failure) \
+    .build(sys.argv)
+
+with open(command_manager.get_p(0)) as file:
     data = json.load(file)
 
 result_path = config.DEFAULT_RESULTS_PATH
 
-dataset = sys.argv[2]
+dataset = command_manager.get_p(1)
 strategy_name = data["strategy"]
 strategy_class = getattr(importlib.import_module("strategies." + strategy_name), strategy_name)
 genome = [Gene(t["lower_bound"], t["upper_bound"], __try_get_json_attr("_value", t)) for t in data["parameters"]]
 hyperparameters = data["hyperparameters"]
 if __name__ == '__main__':
     res = GeneticTrainer.train(strategy_class, genome, dataset,
-                               crossover_operator = __try_get_json_attr("crossover_operator", hyperparameters),
-                               crossover_rate = __try_get_json_attr("crossover_rate", hyperparameters),
-                               mutation_type = __try_get_json_attr("mutation_type", hyperparameters),
-                               mutation_rate = __try_get_json_attr("mutation_rate", hyperparameters),
-                               population_number = __try_get_json_attr("population_number", hyperparameters),
-                               processes_number = __try_get_json_attr("processes_number", hyperparameters),
-                               max_iterations = __try_get_json_attr("max_iterations", hyperparameters))
+                               crossover_operator=__try_get_json_attr("crossover_operator", hyperparameters),
+                               crossover_rate=__try_get_json_attr("crossover_rate", hyperparameters),
+                               mutation_type=__try_get_json_attr("mutation_type", hyperparameters),
+                               mutation_rate=__try_get_json_attr("mutation_rate", hyperparameters),
+                               population_number=__try_get_json_attr("population_number", hyperparameters),
+                               processes_number=__try_get_json_attr("processes_number", hyperparameters),
+                               max_iterations=__try_get_json_attr("max_iterations", hyperparameters))
 
     if res is not None:
         if not os.path.exists(result_path):
@@ -51,5 +63,9 @@ if __name__ == '__main__':
                 print("Something went wrong")
                 sys.exit(1)
 
-        with open(result_path + strategy_name + "_train.res", "w") as outfile:
+        path_key = command_manager.get_k('-o')
+        path = result_path + strategy_name + "_train.res"
+        if path_key is not None:
+            path = path_key
+        with open(path, "w") as outfile:
             outfile.write(res.to_json())
