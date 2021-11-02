b6a681fa66566ef6c5a659d057ebfe2770bdd3d4
Stefano Viel <34980834+stefanoviel@users.noreply.github.com>
2021-11-02 21:31:48 +0100

    finish run

--- run.py
+++ run.py
@@ -2,28 +2,40 @@ import sys
 import importlib
 import json
 from bot import core
+import time
 import datetime
 import config
+from bot.command.command_handler import CommandHandler
 from strategies.StochRsiMacdStrategy import *
 from time import sleep
 from binance import ThreadedWebsocketManager
 
-def update( msg):
+
+def update(msg):
     if msg['e'] != 'error':
-        print(msg)
         strategy.update_state(msg)
     else:
         print('error in reception')
 
 
-with open('option.json') as file:
+def helper(helper_str: str):
+    print(helper_str, flush=True)
+    exit(0)
+
+
+def failure(helper_str: str):
+    print('Wrong syntax \n', flush=True)
+    print(helper_str, flush=True)
+    exit(1)
+
+
+cmd = CommandHandler.create().positional("options").positional("strategy").on_fail(failure).on_help(helper).build(sys.argv)
+
+
+with open(cmd.get_p(0)) as file:
     options = json.load(file)
 
-if len(sys.argv) == 1 or sys.argv[1] == "-h":
-    print("<strategy name>")
-    sys.exit(0)
-
-with open(sys.argv[1]) as file:
+with open(cmd.get_p(1)) as file:
     data = json.load(file)
 
 strategy_name = data["strategy"]
@@ -32,17 +44,12 @@ strategy = strategy_class(BinanceWallet(options, config.API_KEY, config.API_SECR
 
 twm = ThreadedWebsocketManager(api_key=config.API_KEY, api_secret=config.API_SECRET)
 twm.start()
-
-try:
-    twm.start_kline_socket(callback=update, symbol='BTCUSDT')
-except:
-    print('error')
-    exit()
+twm.start_kline_socket(callback=update, symbol=options["first"] + options["second"])
 
 
-
-if input() == 'q':
+if input() == 'q':  # l'errore che genera può essere ignorato
     twm.stop()
+    exit()
 
 
 
