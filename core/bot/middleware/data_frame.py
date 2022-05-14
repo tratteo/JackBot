7a4295aeb088b903d7f639da85c7c9dc7090a925
enrico-car <car.enrico19@gmail.com>
2022-05-14 16:42:23 +0200

    dataset_evaluator middleware

---
+++ core/bot/middleware/data_frame.py
@@ -0,0 +1,13 @@
+from abc import ABC
+
+
+class DataFrame(ABC):
+    def __init__(self):
+        self.symbol = ''
+        self.start_time = 0
+        self.close_time = 0
+        self.open_price = 0
+        self.close_price = 0
+        self.high_price = 0
+        self.low_price = 0
+        self.is_closed = False
---
+++ core/bot/middleware/kucoin_middleware.py
@@ -0,0 +1,40 @@
+import threading
+import time
+from core.bot.middleware.data_frame import DataFrame
+from core.bot.middleware.middleware import MiddleWare
+
+
+# from CexLib.Kucoin.KucoinData import KucoinData
+# importare cartella da branch main/stefano/enrico
+
+class KucoinMiddleWare(MiddleWare):
+
+    def __init__(self, callback, symbol, granularity):
+        super().__init__(callback, symbol, granularity)
+        self.passed_frame = DataFrame()
+        self.unsubscribe = True
+        self.kucoin_data = KucoinData('key', 'secret', 'apiName')  # insert key to subscribe
+        threading.Thread(target=self.subscribe).start()
+
+    def subscribe(self):
+        while self.unsubscribe:
+            self.update(self.kucoin_data.get_candles(self.symbol, self.granularity))
+            time.sleep(int(self.granularity) * 60)
+
+    def convert(self, message) -> DataFrame:
+        frame = DataFrame()
+        frame.symbol = self.symbol
+        frame.start_time = message[198][0]
+        frame.close_time = message[198][0] + (int(self.granularity) * 60000)
+        frame.open_price = message[198][1]
+        frame.close_price = message[198][4]
+        frame.high_price = message[198][2]
+        frame.low_price = message[198][3]
+        frame.is_closed = True
+        return frame
+
+    def stop(self):
+        self.unsubscribe = False
+
+    def update(self, message):
+        self.callback(self.convert(message))
